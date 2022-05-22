bd32bae6ca76b2677f430d8bb46b8284214f243c
ogzhnaldemir <aldemiroguzhan.cs@outlook.com>
2022-05-22 22:15:15 +0300

    tasarıma başlandı.

---
+++ app/src/main/java/com/example/ylandeneme3/GameView.java
@@ -0,0 +1,18 @@
+package com.example.ylandeneme3;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class GameView extends View {
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public  void draw(Canvas canvas){
+        super.draw(canvas);
+    }
+}
+
